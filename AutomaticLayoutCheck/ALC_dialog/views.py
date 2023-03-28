a042455db512677f42efd3441d04abbd2b9aad50
Jald <Artem.Ryzhenkov@yandex.ru>
2023-03-28 21:23:12 +0300

    Add save files feature

--- AutomaticLayoutCheck/ALC_dialog/views.py
+++ AutomaticLayoutCheck/ALC_dialog/views.py
@@ -1,41 +1,39 @@
 from django.core.files.uploadedfile import TemporaryUploadedFile
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .forms import FileUploadForm
 from ALC_dialog.ALC.ALC import ALC
 from django.core.files.storage import FileSystemStorage
 from os import path, getcwd
+import os
+from django.conf import settings
+
+from typing import List, Dict, Union
+from .models import UploadedFile
 
 
-def execute(request):
+def run_check(files_data: List[Dict[str: str]]) -> None:
+    print(files_data)
+    pass
+
+
+def execute(request: WSGIRequest) -> None:
     if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
+        files_data = list()
+        for file_name in request.FILES.keys():
+            data = dict()
+            file = request.FILES[file_name]
+            file_type = file_name.split('.')[-1]
+            uploaded_file = UploadedFile(file=file, name=file_name, type=file_type)
+            uploaded_file.save()
 
-        file1 = _save_file(request.FILES['file1'])
-        file2 = _save_file(request.FILES['file2'])
-
-
-        ALC(file1, file2).exec()
+            file_path = uploaded_file.file.path
+            data['file_name'] = file_name
+            data['file_path'] = file_path
+            files_data.append(data)
+        run_check(files_data)
+        return redirect('ALC_dialog')
     else:
         form = FileUploadForm()
     return render(request, 'main.html', {'form': form})
-
-
-def _save_file(file: TemporaryUploadedFile):
-    # Create an instance of FileSystemStorage to handle file storage
-    fs = FileSystemStorage()
-
-    # Get the file path from the TemporaryUploadedFile object
-    file_path = file.temporary_file_path()
-
-    # Use the FileSystemStorage instance to save the file
-    saved_file_path = fs.save("file", file)
-
-    # You can also delete the TemporaryUploadedFile object
-    # after saving the file
-    file.close()
-    print(saved_file_path)
-    # Return the saved file path
-    return saved_file_path
-
-
-
