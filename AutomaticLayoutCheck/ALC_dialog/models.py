6c81d3f10641866d95aff95ec1a353d7d71d8114
Jald <Artem.Ryzhenkov@yandex.ru>
2023-03-28 21:22:07 +0300

    Modify models

--- AutomaticLayoutCheck/ALC_dialog/models.py
+++ AutomaticLayoutCheck/ALC_dialog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 
 class UploadedFile(models.Model):
-    file = models.FileField()
+    file = models.FileField(upload_to=settings.UPLOADS_FILES_PATH, default=None)
+    name = models.CharField(max_length=255, default=None)
+    type = models.CharField(max_length=10, default=None)
