4f0703ba6334a51fd4994e0183e090fcee6bc09e
Jald <Artem.Ryzhenkov@yandex.ru>
2023-04-09 16:33:31 +0300

    Modify selenium_helper

--- AutomaticLayoutCheck/ALC_dialog/ALC/selenium_helper.py
+++ AutomaticLayoutCheck/ALC_dialog/ALC/selenium_helper.py
@@ -6,32 +6,43 @@ from selenium.webdriver.chrome.options import Options
 from Screenshot import Screenshot_Clipping
 
 
+CHROME_DRIVER_PATH = path.join(getcwd(), 'drivers', 'chromedriver.exe')
+
+
 class SeleniumHelper(object):
     """Класс для работы с Selenium"""
 
     SITE_SCREENSHOT_NAME = 'sample.png'
 
-    def __init__(self, components_path: str, save_path: str, driver_path: str):
-        "добавить доку"
-        self.components_path = components_path
-        self.save_path = save_path
-        self.driver_path = driver_path
+    def __init__(self, browser_width: int = None, browser_height: int = None) -> None:
+        """Инициализация класса.
+
+        Args:
+            browser_width: ширина окна браузера.
+            browser_height: высота окна браузера.
+        """
+        self.driver_path = CHROME_DRIVER_PATH
 
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--start-maximized')
+        if browser_width is not None or browser_height is not None:
+            options.add_argument(f'--window-size={browser_width},{browser_height}')
 
         driver = webdriver.Chrome(executable_path=self.driver_path, chrome_options=options)
         self.driver = driver
 
-    @property
-    def get_full_screenshot_page(self) -> str:
-        """Получение скриншота полностью всей страницы.
+    def get_full_screenshot_page(self, index_path: str, save_img_path: str) -> str:
+        """Получение скриншота всей страницы.
+
+        Args:
+            index_path: путь до изображения.
+            save_img_path: путь для сохранения.
 
         Returns:
             Путь с сохраненным скриншотом.
         """
-        self.driver.get(self.components_path)
+        self.driver.get(index_path)
         return Screenshot_Clipping.Screenshot().full_Screenshot(driver=self.driver,
-                                                                save_path=self.save_path,
+                                                                save_path=save_img_path,
                                                                 image_name=self.SITE_SCREENSHOT_NAME)
