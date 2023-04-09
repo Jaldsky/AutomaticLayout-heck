8d4daa36b157ad5f152954ddb64a16e91f3beadf
Jald <Artem.Ryzhenkov@yandex.ru>
2023-04-09 16:24:20 +0300

    Modify image_helper

--- AutomaticLayoutCheck/ALC_dialog/ALC/image_helper.py
+++ AutomaticLayoutCheck/ALC_dialog/ALC/image_helper.py
@@ -1,29 +1,39 @@
-from os import path, getcwd
+from typing import Tuple
+from os import path
+
 from PIL import Image
 from psd_tools import PSDImage
 
 
 class ImageHelper(object):
-    """Класс для конвертации формата изображений"""
+    """Класс для работы с изображениями"""
 
     PIC_NAME = 'reference_sample.png'
 
-    def convert_psd_to_png(self, path_to_pic: str, path_to_save: str) -> str:
+    def convert_psd_to_png(self, path_to_img: str, path_to_save: str) -> str:
         """Конвертация формата psd в png.
 
         Args:
-            path_to_pic: путь до изображения.
+            path_to_img: путь до изображения.
             path_to_save: путь для сохранения.
 
         Returns:
             Путь с сохраненным сконвертированным изображением.
         """
-        psd_pic = PSDImage.open(path_to_pic)
+        psd_pic = PSDImage.open(path_to_img)
         save_path = path.join(path_to_save, self.PIC_NAME)
         psd_pic.composite().save(save_path)
         return save_path
 
     @staticmethod
-    def get_image_resolution(path_to_pic):
-        with Image.open(path_to_pic) as img:
+    def get_image_resolution(path_to_img: str) -> Tuple[int, int]:
+        """Функция получения разрешения изображения.
+
+        Args:
+            path_to_img: путь до изображения.
+
+        Returns:
+            Кортеж со значением ширины и высоты.
+        """
+        with Image.open(path_to_img) as img:
             return img.size
