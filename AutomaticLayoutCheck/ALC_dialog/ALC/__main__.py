c0e222599884d8687c23a9f10fdc0579babfcfce
Jald <Artem.Ryzhenkov@yandex.ru>
2023-04-15 17:42:03 +0300

    Delete files

--- AutomaticLayoutCheck/ALC_dialog/ALC/__main__.py
+++
@@ -1,6 +0,0 @@
-from selenium_helper import SeleniumHelper
-from pic_format_converter import PicFormatConverter
-
-SeleniumHelper().get_full_screenshot_page()
-PicFormatConverter().convert_psd_to_png()
-
