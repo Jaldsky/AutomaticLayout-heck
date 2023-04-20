a1b10d115ed037a9dcc2cabbb941b57eab9edfd7
Artem <RyzhenkovArt@yandex.ru>
2023-04-20 23:13:40 +0500

    Add bedaub_text()

--- AutomaticLayoutCheck/ALC_dialog/ALC/image_helper.py
+++ AutomaticLayoutCheck/ALC_dialog/ALC/image_helper.py
@@ -1,15 +1,18 @@
-from typing import Tuple
+from typing import Tuple, List
 from os import path
 
 from PIL import Image
 from psd_tools import PSDImage
+from cv2 import imread, imwrite, rectangle, FILLED
+
+import easyocr
 
 
 PIC_NAME = 'reference_sample.png'
 
 
 class ImageHelper(object):
-    """Класс для работы с изображениями"""
+    """Класс для работы с изображениями."""
 
     def convert_psd_to_png(self, path_to_img: str, path_to_save: str) -> str:
         """Конвертация формата psd в png.
@@ -38,3 +41,32 @@ class ImageHelper(object):
         """
         with Image.open(path_to_img) as img:
             return img.size
+
+    @staticmethod
+    def bedaub_text(path_to_img: str, path_to_save: str, languages=None):
+        """Функция поиска и замазки текста.
+
+        Args:
+            path_to_img: путь до изображения.
+            path_to_save: путь для сохранения.
+            languages: используемые языки на изображении.
+
+        Returns:
+            Путь с сохраненным замазаным текстом изображением.
+        """
+        if languages is None:
+            languages = ['en', 'ru']
+
+        image = imread(path_to_img)
+
+        reader = easyocr.Reader(languages, gpu=True)
+        results = reader.readtext(image)
+
+        for result in results:
+            bbox = result[0]
+            x1, y1 = int(bbox[0][0]), int(bbox[0][1])
+            x2, y2 = int(bbox[2][0]), int(bbox[2][1])
+            rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), FILLED)
+
+        imwrite(path_to_save, image)
+        return path_to_save
