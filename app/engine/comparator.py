b736233694baaa24a27432628091a6298f58e3d9
NordN <RyzhenkovArt@yandex.ru>
2024-03-06 21:50:39 +0500

    Refactor comparator.py part 2

--- app/engine/comparator.py
+++ app/engine/comparator.py
@@ -1,18 +1,11 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Tuple
+
+from numpy import ndarray, mean, expand_dims
 from skimage.metrics import structural_similarity
-import cv2
-# from cv2 import imread, resize, cvtColor, cvtColor, COLOR_BGR2RGB, COLOR_BGR2GRAY
 from sklearn.metrics.pairwise import cosine_similarity
-
-from keras.applications.resnet import preprocess_input
 from keras.applications.vgg16 import VGG16
-
-
-# from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from functools import cached_property
-from numpy import ndarray
+from keras.applications.resnet import preprocess_input
 
 from app.engine.image_helper import ImageHelper
 
@@ -20,6 +13,8 @@ from app.engine.image_helper import ImageHelper
 @dataclass
 class ComparatorInterface(ABC):
     """Interface for comparing image similarity."""
+    reference_image_path: str
+    image_path: str
 
     @staticmethod
     @abstractmethod
@@ -38,6 +33,7 @@ class ComparatorInterface(ABC):
 @dataclass
 class ComparatorBase(ComparatorInterface):
     """Base class for comparing image similarity."""
+    image_size: tuple[int, int]
     similarity_threshold: float
     image_helper: ImageHelper = ImageHelper()
 
@@ -46,17 +42,18 @@ class ComparatorBase(ComparatorInterface):
         """Method for performing image similarity comparison."""
 
     @staticmethod
-    def get_similarity_percentages(compare_index: float) -> float:
+    def get_similarity_percentages(compare_index: float, round_value: int = 2) -> float:
         """Method for obtaining the percentage of similarity between two images.
 
         Args:
             compare_index: image similarity index.
+            round_value: rounding value.
 
         Returns:
             Percentage of similarity between images.
         """
         # TODO add unit test
-        return compare_index * 100
+        return round(compare_index * 100, round_value)
 
     def are_images_similar(self, compare_index: float) -> bool:
         """Method for obtaining an assertion of image similarity.
@@ -68,7 +65,7 @@ class ComparatorBase(ComparatorInterface):
             Compare the images and determine if they are similar.
         """
         # TODO add unit test
-        return compare_index <= self.similarity_threshold
+        return compare_index >= self.similarity_threshold
 
     def prepare_comparison_image(self, image_path: str,
                                  image_size: tuple[int, int],
@@ -86,7 +83,6 @@ class ComparatorBase(ComparatorInterface):
         Returns:
             Pre-processed image(pixel matrix) is ready for comparison.
         """
-        # TODO add unit test
         image = self.image_helper.read_image(image_path)
 
         image = self.image_helper.convert_image_bgr_to_rgb(image)
@@ -97,189 +93,67 @@ class ComparatorBase(ComparatorInterface):
         return image
 
 
+@dataclass
 class ComparatorMeanSquaredError(ComparatorBase):
-    """Класс для сравнения схожести двух изображений по методу среднеквадратичной ошибки (Mean Squared Error - MSE)."""
+    """Class for comparing image similarity using the Mean Squared Error - MSE."""
+    image_size: tuple[int, int] = (500, 500)
+    similarity_threshold: float = 0.45
 
-    def __init__(self, reference_img_path: str, sample_img_path: str, img_size: Tuple = (500, 500),
-                 threshold: float = 0.45) -> None:
-        """Инициализация класса.
-
-        Args:
-            reference_img_path: путь до эталонного изображения.
-            sample_img_path: путь до сравниваемого изображения.
-            img_size: размер изображения.
-            threshold: попрог определения схожести изображений.
-        """
-        super().__init__()
-        self.reference_img_path = reference_img_path
-        self.sample_img_path = sample_img_path
-        self.img_size = img_size
-        self.threshold = threshold
-
-    @property
-    def are_images_similar(self) -> str:
-        """Функция проверки схожести изображений.
-
-        Returns:
-            True - похожи, False - не похожи.
-        """
-        nrmse = self.compare_exec
-        if nrmse <= self.threshold:
-            return 'Нет'
-        else:
-            return 'Да'
-
-    @property
-    def get_similarity_percentages(self) -> float:
-        """Получить процент схожести двух изображений.
-
-        Returns:
-            Процент схожести двух изображений.
-        """
-        mse = self.compare_exec
-        similarity_percentage = mse * 100
-        return similarity_percentage
-
-    @cached_property
     def compare_exec(self) -> float:
-        """Выполнить сравнение схожести двух изображений.
+        """Method for performing image similarity comparison..
 
         Returns:
-            Схожесть двух изображений в индексе.
+            Image similarity index.
         """
-        reference_img = self.prepare_image(self.reference_img_path, self.img_size)
-        sample_img = self.prepare_image(self.sample_img_path, self.img_size)
-
-        # calculate the root mean square error (MSE)
-        mse = np.mean((reference_img - sample_img) ** 2)
-        return 1 - (mse / 100)
+        reference_image = self.prepare_comparison_image(self.reference_image_path, image_size=self.image_size)
+        image = self.prepare_comparison_image(self.image_path, image_size=self.image_size)
+        return 1 - (mean((reference_image - image) ** 2) / 100)
 
 
+@dataclass
 class ComparatorStructuralSimilarityIndex(ComparatorBase):
-    """Класс для сравнения схожести двух изображений по методу
-    индексу структурного подобия (Structural Similarity Index - SSIM)."""
+    """Class for comparing image similarity using Structural Similarity Index Measure - SSIM."""
+    image_size: tuple[int, int] = (500, 500)
+    similarity_threshold: float = 0.5
 
-    def __init__(self, reference_img_path: str, sample_img_path: str, img_size: Tuple = (500, 500),
-                 threshold: float = 0.5) -> None:
-        """Инициализация класса.
-
-        Args:
-            reference_img_path: путь до эталонного изображения.
-            sample_img_path: путь до сравниваемого изображения.
-            threshold: попрог определения схожести изображений.
-        """
-        super().__init__()
-        self.reference_img_path = reference_img_path
-        self.sample_img_path = sample_img_path
-        self.img_size = img_size
-        self.threshold = threshold
-
-    @property
-    def are_images_similar(self) -> str:
-        """Функция проверки схожести изображений.
-
-        Returns:
-            Да - похожи, Нет - не похожи.
-        """
-        nrmse = self.compare_exec
-        if nrmse <= self.threshold:
-            return 'Нет'
-        else:
-            return 'Да'
-
-    @property
-    def get_similarity_percentages(self) -> float:
-        """Получить процент схожести изображений.
-
-        Returns:
-            Процент схожести.
-        """
-        ssim = self.compare_exec
-        similarity_percentage = ssim * 100
-        return similarity_percentage
-
-    @cached_property
     def compare_exec(self) -> float:
-        """Выполнить сравнение схожести двух изображений.
+        """Method for performing image similarity comparison..
 
         Returns:
-            Схожесть двух изображений в индексе.
+            Image similarity index.
         """
-        reference_img = self.prepare_image(self.reference_img_path, self.img_size)
-        sample_img = self.prepare_image(self.sample_img_path, self.img_size)
-
-        # Calculate the SSIM score
-        ssim_score = structural_similarity(reference_img, sample_img)
-        return ssim_score
+        reference_image = self.prepare_comparison_image(self.reference_image_path, image_size=self.image_size)
+        image = self.prepare_comparison_image(self.image_path, image_size=self.image_size)
+        return structural_similarity(reference_image, image)
 
 
+@dataclass
 class ComparatorNeuralNetworkVGG16(ComparatorBase):
-    """Класс для сравнения схожести двух изображений на основе нейросети VGG16."""
+    """Class for comparing the similarity of two images based on the VGG16 neural network."""
+    image_size: tuple[int, int] = (224, 224)
+    similarity_threshold: float = 0.8
 
-    def __init__(self, reference_img_path: str, sample_img_path: str, img_size: Tuple = (224, 224),
-                 threshold: float = 0.8) -> None:
-        """Инициализация класса.
-
-        Args:
-            reference_img_path: путь до эталонного изображения.
-            sample_img_path: путь до сравниваемого изображения.
-            threshold: попрог определения схожести изображений.
-        """
-        super().__init__()
-        self.reference_img_path = reference_img_path
-        self.sample_img_path = sample_img_path
-        self.img_size = img_size
-        self.threshold = threshold
-
-    @property
-    def are_images_similar(self) -> str:
-        """Функция проверки схожести изображений.
-
-        Returns:
-            True - похожи, False - не похожи.
-        """
-        nrmse = self.compare_exec
-        if nrmse <= self.threshold:
-            return 'Нет'
-        else:
-            return 'Да'
-
-    @property
-    def get_similarity_percentages(self) -> float:
-        """Получить процент схожести изображений.
-
-        Returns:
-            Процент схожести.
-        """
-        sim = self.compare_exec
-        similarity_percentage = sim * 100
-        return similarity_percentage
-
-    @cached_property
     def compare_exec(self) -> float:
-        """Выполнить сравнение схожести двух изображений.
+        """Method for performing image similarity comparison..
 
         Returns:
-            Схожесть двух изображений в индексе.
+            Image similarity index.
         """
-        reference_img = self.prepare_image(self.reference_img_path, self.img_size, convert_grayscale=False)
-        sample_img = self.prepare_image(self.sample_img_path, self.img_size, convert_grayscale=False)
+        reference_image = self.prepare_comparison_image(self.reference_image_path,
+                                                        image_size=self.image_size,
+                                                        convert_grayscale=False)
+        image = self.prepare_comparison_image(self.image_path,
+                                              image_size=self.image_size,
+                                              convert_grayscale=False)
 
-        # Load pre-trained CNN
-        cnn = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+        cnn = VGG16(weights='imagenet', include_top=False, input_shape=tuple([*self.image_size, 3]))
 
-        # Expand dimensions to match input shape of CNN
-        x1 = np.expand_dims(reference_img, axis=0)
-        x2 = np.expand_dims(sample_img, axis=0)
+        reference_image = expand_dims(reference_image, axis=0)
+        image = expand_dims(image, axis=0)
 
-        # Normalize pixel values
-        x1 = preprocess_input(x1)
-        x2 = preprocess_input(x2)
+        reference_image = preprocess_input(reference_image)
+        image = preprocess_input(image)
 
-        # Extract features from images using CNN
-        features1 = cnn.predict(x1)
-        features2 = cnn.predict(x2)
-
-        # Compute cosine similarity between features
-        vgg16 = cosine_similarity(features1.reshape(1, -1), features2.reshape(1, -1))[0][0]
-        return vgg16
+        features1 = cnn.predict(reference_image)
+        features2 = cnn.predict(image)
+        return cosine_similarity(features1.reshape(1, -1), features2.reshape(1, -1))[0][0]
